8502d0e272ad0b496d1b4b5cdebf01bac76b8007
sana197111 <yc0047@gmail.com>
2020-12-04 13:33:53 +0000

    Updates

---
+++ precache-manifest.742dfd5ed06b1269f1a949054687e560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "0999027e7fd42576948d",
+    "url": "/movie_app/static/js/main.0999027e.chunk.js"
+  },
+  {
+    "revision": "b1683011c8a66d0978c4",
+    "url": "/movie_app/static/js/1.b1683011.chunk.js"
+  },
+  {
+    "revision": "0999027e7fd42576948d",
+    "url": "/movie_app/static/css/main.c099ad56.chunk.css"
+  },
+  {
+    "revision": "2d214cdaffe325e99f8a38adc238efa8",
+    "url": "/movie_app/index.html"
+  }
+];
